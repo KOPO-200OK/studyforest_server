cdd061394270f8a307fcda129e57d3bc0b878f00
KIM HUIJU <gmlwnchlrh@gmail.com>
2026-07-13 11:07:41 +0900

    fix: 운영 도메인 CORS 허용

--- src/main/java/com/gongsoop/global/config/SecurityConfig.java
+++ src/main/java/com/gongsoop/global/config/SecurityConfig.java
@@ -108,7 +108,9 @@ public class SecurityConfig {
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
                 "http://localhost:3000",
-                "http://127.0.0.1:3000"
+                "http://127.0.0.1:3000",
+                "https://studyforest.site",
+                "https://www.studyforest.site"
         ));
 
         configuration.setAllowedMethods(List.of(
--- src/main/java/com/gongsoop/global/config/WebSocketConfig.java
+++ src/main/java/com/gongsoop/global/config/WebSocketConfig.java
@@ -31,7 +31,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                         "http://localhost:5173",
                         "http://127.0.0.1:5173",
                         "http://localhost:3000",
-                        "http://127.0.0.1:3000"
+                        "http://127.0.0.1:3000",
+                        "https://studyforest.site",
+                        "https://www.studyforest.site"
                 );
     }
 
